feda5477fefb20677c1fadfb5f1a0106d6bc316e
Kennywes <Kennywest844@gmail.com>
2024-12-17 00:10:37 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 
-global.pairingNumber = "50943420186"
+global.pairingNumber = "50932589537"
  
-global.botNumber = ["50943420186"]
+global.botNumber = ["50932589537"]
 
 global.anticall = false
 
